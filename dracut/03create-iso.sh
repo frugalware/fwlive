5fc9b1935e6e72c4004ed1413d3e8fb7cf7c1b59
Miklos Vajna <vmiklos@frugalware.org>
2011-10-30 13:34:32 +0100

    simplify cd label

--- dracut/03create-iso.sh
+++ dracut/03create-iso.sh
@@ -3,4 +3,4 @@
 rm -rf iso
 mkdir -p iso/LiveOS
 cp -v squashfs.img iso/LiveOS
-mkisofs -R -J -V frugal-minim-x86_64-201110300111 -o live.iso iso
+mkisofs -R -J -V fwlive -o fwlive.iso iso
--- dracut/04boot-qemu.sh
+++ dracut/04boot-qemu.sh
@@ -1,2 +1,2 @@
 qemu-system-x86_64 -enable-kvm -m 512 -kernel rootfs/boot/vmlinuz -initrd rootfs/boot/initrd.img.xz \
-	-append "root=live:CDLABEL=frugal-minim-x86_64-201110300111 rd.driver.pre=loop rd.plymouth=0" -cdrom live.iso
+	-append "root=live:CDLABEL=fwlive rd.driver.pre=loop rd.plymouth=0" -cdrom fwlive.iso
