b1b496d2a9b53c69468fa467478771969bbf4bf5
Miklos Vajna <vmiklos@frugalware.org>
2011-10-30 13:32:33 +0100

    drop not necessary kernel params

--- dracut/04boot-qemu.sh
+++ dracut/04boot-qemu.sh
@@ -1,1 +1,2 @@
-qemu-system-x86_64 -enable-kvm -m 512 -kernel rootfs/boot/vmlinuz -initrd rootfs/boot/initrd.img.xz -append "root=live:CDLABEL=frugal-minim-x86_64-201110300111 rootfstype=auto ro liveimg rd.luks=0 rd.md=0 rd.dm=0 rd.driver.pre=loop" -cdrom live.iso
+qemu-system-x86_64 -enable-kvm -m 512 -kernel rootfs/boot/vmlinuz -initrd rootfs/boot/initrd.img.xz \
+	-append "root=live:CDLABEL=frugal-minim-x86_64-201110300111 rd.driver.pre=loop rd.plymouth=0" -cdrom live.iso
