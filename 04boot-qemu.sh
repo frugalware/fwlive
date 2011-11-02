2f7c0e5976b8f30def2c9d9643a081116a3a0b6d
Miklos Vajna <vmiklos@frugalware.org>
2011-11-02 23:55:23 +0100

    04boot-qemu.sh: use config

--- 04boot-qemu.sh
+++ 04boot-qemu.sh
@@ -1,2 +1,4 @@
-qemu-system-x86_64 -enable-kvm -m 512 -kernel rootfs/boot/vmlinuz -initrd rootfs/boot/initrd.img.xz \
-	-append "root=live:CDLABEL=fwlive rd.driver.pre=loop rd.plymouth=0" -cdrom fwlive.iso
+source ./config
+
+qemu-system-x86_64 -enable-kvm -m 512 -kernel $CHROOTDIR/boot/vmlinuz -initrd $CHROOTDIR/boot/initrd.img.xz \
+	-append "root=live:CDLABEL=fwlive rd.driver.pre=loop rd.plymouth=0" -cdrom fwlive-$TREE.iso
