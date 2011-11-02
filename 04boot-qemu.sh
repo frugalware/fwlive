b205dff4bd8f969b0b164a35354d25053725b628
Miklos Vajna <vmiklos@frugalware.org>
2011-11-02 23:57:11 +0100

    no need to duplicate kernel params here

--- 04boot-qemu.sh
+++ 04boot-qemu.sh
@@ -1,4 +1,3 @@
 source ./config
 
-qemu-system-x86_64 -enable-kvm -m 512 -kernel $CHROOTDIR/boot/vmlinuz -initrd $CHROOTDIR/boot/initrd.img.xz \
-	-append "root=live:CDLABEL=fwlive rd.driver.pre=loop rd.plymouth=0" -cdrom fwlive-$TREE.iso
+qemu-system-x86_64 -enable-kvm -m 512 -cdrom fwlive-$TREE.iso
